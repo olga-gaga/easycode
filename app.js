a172f51c239873f487752e9a4bf0c42a2fdd0bc2
Olga Garbuz <garbuz9112000@gmail.com>
2020-11-27 23:18:53 +0200

    first_project

--- app.js
+++ app.js
@@ -1,154 +1,334 @@
-//task 1
-console.log('task 1');
+// Форма
+// Список задач
+const tasks = [
+  {
+    _id: '5d2ca9e2e03d40b326596aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095c1288e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+  {
+    _id: '5d2ca9e2e03d40b3232496aa7',
+    completed: true,
+    body:
+      'Occaecat non ea quis occaecat ad culpa amet deserunt incididunt elit fugiat pariatur. Exercitation commodo culpa in veniam proident laboris in. Excepteur cupidatat eiusmod dolor consectetur exercitation nulla aliqua veniam fugiat irure mollit. Eu dolor dolor excepteur pariatur aute do do ut pariatur consequat reprehenderit deserunt.\r\n',
+    title: 'Eu ea incididunt sunt consectetur fugiat non.',
+  },
+  {
+    _id: '5d2ca9e29c8a94095564788e0',
+    completed: false,
+    body:
+      'Aliquip cupidatat ex adipisicing veniam do tempor. Lorem nulla adipisicing et esse cupidatat qui deserunt in fugiat duis est qui. Est adipisicing ipsum qui cupidatat exercitation. Cupidatat aliqua deserunt id deserunt excepteur nostrud culpa eu voluptate excepteur. Cillum officia proident anim aliquip. Dolore veniam qui reprehenderit voluptate non id anim.\r\n',
+    title:
+      'Deserunt laborum id consectetur pariatur veniam occaecat occaecat tempor voluptate pariatur nulla reprehenderit ipsum.',
+  },
+];
 
-(function (){
+(function(arrOfTasks) {
+  const objOfTasks = arrOfTasks.reduce((acc, task) => {
+    acc[task._id] = task;
+    return acc;
+  }, {});
+  console.log(objOfTasks);
 
-    const map = ["_id", "name", "isActive", "balance"];
-
-    const users = [
-    {
-        "_id": "5d220b10e8265cc978e2586b",
-        "isActive": true,
-        "balance": 2853.33,
-        "age": 20,
-        "name": "Buckner Osborne",
-        "gender": "male",
-        "company": "EMPIRICA",
-        "email": "bucknerosborne@empirica.com",
-        "phone": "+1 (850) 411-2997",
-        "registered": "2018-08-13T04:28:45 -03:00"
+  const themes = {
+    default: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#007bff',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#007bff',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#0069d9',
+      '--default-btn-border-color': '#0069d9',
+      '--danger-btn-bg': '#dc3545',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#bd2130',
+      '--danger-btn-border-color': '#dc3545',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#80bdff',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(0, 123, 255, 0.25)',
     },
-    {
-        "_id": "5d220b10144ef972f6c2b332",
-        "isActive": true,
-        "balance": 1464.63,
-        "age": 38,
-        "name": "Rosalie Smith",
-        "gender": "female",
-        "company": "KATAKANA",
-        "email": "rosaliesmith@katakana.com",
-        "phone": "+1 (943) 463-2496",
-        "registered": "2016-12-09T05:15:34 -02:00"
+    dark: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#343a40',
+      '--header-text-color': '#fff',
+      '--default-btn-bg': '#58616b',
+      '--default-btn-text-color': '#fff',
+      '--default-btn-hover-bg': '#292d31',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#b52d3a',
+      '--danger-btn-text-color': '#fff',
+      '--danger-btn-hover-bg': '#88222c',
+      '--danger-btn-border-color': '#88222c',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
     },
-    {
-        "_id": "5d220b1083a0494655cdecf6",
-        "isActive": false,
-        "balance": 2823.39,
-        "age": 40,
-        "name": "Estrada Davenport",
-        "gender": "male",
-        "company": "EBIDCO",
-        "email": "estradadavenport@ebidco.com",
-        "phone": "+1 (890) 461-2088",
-        "registered": "2016-03-04T03:36:38 -02:00"
+    light: {
+      '--base-text-color': '#212529',
+      '--header-bg': '#fff',
+      '--header-text-color': '#212529',
+      '--default-btn-bg': '#fff',
+      '--default-btn-text-color': '#212529',
+      '--default-btn-hover-bg': '#e8e7e7',
+      '--default-btn-border-color': '#343a40',
+      '--default-btn-focus-box-shadow':
+        '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+      '--danger-btn-bg': '#f1b5bb',
+      '--danger-btn-text-color': '#212529',
+      '--danger-btn-hover-bg': '#ef808a',
+      '--danger-btn-border-color': '#e2818a',
+      '--input-border-color': '#ced4da',
+      '--input-bg-color': '#fff',
+      '--input-text-color': '#495057',
+      '--input-focus-bg-color': '#fff',
+      '--input-focus-text-color': '#495057',
+      '--input-focus-border-color': '#78818a',
+      '--input-focus-box-shadow': '0 0 0 0.2rem rgba(141, 143, 146, 0.25)',
+    },
+  };
+
+  let lastSelectedTheme = 'default';
+  let sortTask = 'all';
+
+  // Elemnts UI
+  const listContainer = document.querySelector(
+    '.tasks-list-section .list-group',
+  );
+  const form = document.forms['addTask'];
+  const inputTitle = form.elements['title'];
+  const inputBody = form.elements['body'];
+  const themeSelect = document.getElementById('themeSelect');
+  const sortTasksSelect = document.querySelector('.sort');
+
+  // Events
+  renderAllTasks(objOfTasks);
+  form.addEventListener('submit', onFormSubmitHandler);
+  listContainer.addEventListener('click', onButtonLiHandler);
+  themeSelect.addEventListener('change', onThemeSelectHandler);
+  sortTasksSelect.addEventListener('click', onSortSelectHandler);
+
+
+  function renderAllTasks(tasksList) {
+    if (!tasksList) {
+      console.error('Передайте список задач!');
+      return;
     }
-    ];
-
-    const mapUsers = users.map(user => 
-        map.reduce((acc, key) => {
-            if (Object.keys(user).indexOf(key) !== -1) {
-                acc[key] = user[key];
-            }
-            return acc;
-        }, {}));
-
-    console.log(mapUsers);
-
-})();
-//task 2
-console.log('task 2');
-
-(function() {
-
-    const charWithIndex = [{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2}, {char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0}, {char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}];
-
-    function createStr(arrayOfChars){
-        if(Array.isArray(arrayOfChars)){
-            return [...arrayOfChars]
-                .sort((prevIndex, nextIndex) => prevIndex.index - nextIndex.index)
-                .reduce((acc, char) => (acc += char.char), '');
-        }
-        else console.log('Ошибочное значение');
+    else if (Object.values(tasksList).length === 0) { //вывод сообщения, что список задач пуст
+      renderEmptyListMessage();
+      return;
     }
 
-    const resultStr = createStr(charWithIndex);
+    const fragment = document.createDocumentFragment();
+    Object.values(tasksList)
+    .sort( (prevValue, nextValue) => prevValue.completed - nextValue.completed)
+    .forEach(task => {
+      const li = listItemTemplate(task);
+      fragment.appendChild(li);
+    });
+    listContainer.appendChild(fragment);
+  }
 
-    console.log(resultStr);
+  function renderEmptyListMessage(){
+    const emptyTaskListMessage = document.createElement('p');
+    emptyTaskListMessage.textContent = 'Список задач пуст!';
+    emptyTaskListMessage.style.color = 'red';
+    form.insertAdjacentElement('afterend', emptyTaskListMessage);
+  }
 
-})();
+  function listItemTemplate({ _id, title, body, completed } = {}) {
+    const li = document.createElement('li');
+    li.classList.add(
+      'list-group-item',
+      'd-flex',
+      'align-items-center',
+      'flex-wrap',
+      'mt-2',
+    );
+    li.setAttribute('data-task-id', _id);
 
-//task 3
-console.log('task 3');
+    const span = document.createElement('span');
+    span.textContent = title;
+    span.style.fontWeight = 'bold';
 
-(function() {
-    
-    function getInfo ({name = 'Unknown', info: {partners: [partner1, partner2] = []} = {}} = {}){
-        console.log(`Name: ${name} \nPartners: ${partner1}, ${partner2}`);
+    const deleteBtn = document.createElement('button');
+    deleteBtn.textContent = 'Delete task';
+    deleteBtn.classList.add('btn', 'btn-danger', 'col-sm', 'offset-md-3', 'delete-btn');
+
+    const completeBtn = document.createElement('button'); //кнопка выполнения
+    completeBtn.classList.add('btn', 'ml-auto', 'col-sm', 'offset-md-3', 'complete-btn');
+    changeCompleteTaskHtml(completed, li, span,completeBtn); 
+
+    const div = document.createElement('div');
+    div.classList.add('buttons', 'row', 'col-md-8', 'ml-auto');
+    div.appendChild(completeBtn);
+    div.appendChild(deleteBtn);
+
+    const article = document.createElement('p');
+    article.textContent = body;
+    article.classList.add('mt-2', 'w-100');
+
+    li.appendChild(span);
+    li.appendChild(div);
+    li.appendChild(article);
+    return li;
+  }
+
+  function onFormSubmitHandler(e) {
+    e.preventDefault();
+    const titleValue = inputTitle.value;
+    const bodyValue = inputBody.value;
+
+    if (!titleValue || !bodyValue) {
+      alert('Пожалуйста введите title и body');
+      return;
     }
 
-    const organisation = { 
-        name: 'Google', 
-        info: { 
-          employees: ['Vlad', 'Olga'], 
-          partners: ['Microsoft', 'Facebook', 'Xing'] 
-      } };
+    const task = createNewTask(titleValue, bodyValue);
+    const listItem = listItemTemplate(task);
+    listContainer.insertAdjacentElement('afterbegin', listItem);
+    form.reset();
+  }
 
-    getInfo(organisation);
-
-})();
-
-//task 4
-console.log('task 4');
-
-(function() {  
-
-    const person = {
-        name: 'Denis',
-        age: 30,
-        lastGet: '',
-        lastUpdate: '', 
-        get nameField(){
-            this.lastGet = Date(Date.now());
-            console.log(this.name);
-        }, 
-        set nameField (newName) {
-            this.lastUpdate = Date(Date.now());
-            if(typeof newName === 'string') this.name = newName;
-            else console.log('Ошибочное значение');
-        }
+  function createNewTask(title, body) {
+    const newTask = {
+      title,
+      body,
+      completed: false,
+      _id: `task-${Math.random()}`,
     };
-    console.log(person);
-    person.nameField;
-    console.log(person);
-    person.nameField = 'Olga';
-    console.log(person);
-})();
 
-//task 5
-console.log('task 5');
+    objOfTasks[newTask._id] = newTask;
 
-(function() {
-    
-    const product = {
-        brand: 'Apple',
-        model: 'iPhone 7',
-        price: '$300', 
-        get info() {
-            return `${this.brand} ${this.model}`;
-        },
-        set info(value) {
-            if(typeof value === 'string') {
-                [this.brand, ...model] = value.split(' ');
-                this.model = model.join(' ');
-            }
-            else console.log('Ошибочное значение');
-        },
-    };
-    console.log(product);
-    console.log(product.info);
-    product.info = 'Samsung S8 Gold';
-    console.log(product);
-    console.log(product.info);
+    return { ...newTask };
+  }
 
-})();
+  function onButtonLiHandler({target}) {
+    if(target.tagName = 'BUTTON') {
+      const parent = target.closest('[data-task-id]');
+      const id = parent.dataset.taskId;
+      if(target.classList.contains('delete-btn')) onDeleteHandler(id, parent);
+      else if (target.classList.contains('complete-btn')) onCompleteHandler(id, target);
+  
+    }
+  }
 
+  function onDeleteHandler(id, parent) {
+    const confirmed = deleteTask(id);
+    deleteTaskFromHtml(confirmed, parent);
+    if (Object.values(objOfTasks).length === 0) renderEmptyListMessage(); //вывод сообщения, что список задач пуст
+  }
+
+  function onCompleteHandler(id, target) {
+    const complited = completeTask(id); //изменяет поле complited 
+    const li =  target.closest('li');
+    const span = li.firstChild;
+    changeCompleteTaskHtml(complited, li, span, target);
+    if (sortTask === 'completed') setTimeout(deleteTaskFromHtml, 500, true, li);
+  }
+
+  function deleteTask(id) {
+    const { title } = objOfTasks[id];
+    const isConfirm = confirm(`Точно вы хотите удалить задачу: ${title}`);
+    if (!isConfirm) return isConfirm;
+    delete objOfTasks[id];
+    return isConfirm;
+  }
+
+  function deleteTaskFromHtml(confirmed, el) {
+    if (!confirmed) return;
+    el.remove();
+  }
+
+  function completeTask(id){
+    objOfTasks[id].completed = objOfTasks[id].completed ? false : true;
+    console.log(objOfTasks[id].completed);
+    return objOfTasks[id].completed;
+  }
+
+  //изменяет вид и содержимое кнопки в зависимости от статуса задачи
+  function changeCompleteTaskHtml(completed, item, title, button){ 
+    if (completed) {
+      item.classList.add('bg-light');
+      title.classList.add('text-success');
+      button.classList.add('btn-warning');
+      button.classList.remove('btn-success');
+      button.textContent = 'Not completed';
+    }
+    else{
+      item.classList.remove('bg-light');
+      title.classList.remove('text-success');
+      button.classList.add('btn-success');
+      button.classList.remove('btn-warning');
+      button.textContent = 'Completed';
+    }
+  }
+
+  //изменяет вид кнопки сортировки
+  function changeSortButtonStyle(newChoice, prevChoice) {
+    prevChoice.classList.replace('btn-primary', 'btn-secondary');
+    newChoice.classList.replace('btn-secondary', 'btn-primary');
+  }
+
+  function renderSortTaskList(sortTask, tasksList){
+    listContainer.innerHTML = '';
+    if (sortTask === 'all') renderAllTasks(tasksList);
+    else {
+      //объект только с выполненными задачами
+      completedTasksList = Object.values(tasksList)
+        .reduce ((acc, task) => {
+          if ( task.completed ) acc[task._id] = task; 
+          return acc;
+        }, {});
+      
+      renderAllTasks(completedTasksList);
+    }
+    return;
+  }
+
+  function onSortSelectHandler ({target}){
+    if (!target.classList.contains('sort-btn') || sortTask === target.value) return;
+    changeSortButtonStyle(target, sortTasksSelect.querySelector(`[value="${sortTask}"]`));
+    sortTask = target.value;
+    renderSortTaskList(sortTask, objOfTasks);
+  }
+
+  function onThemeSelectHandler(event) {
+    const selectedTheme = themeSelect.value;
+    const isConfirmed = confirm(`Вы действительно хотите поменять текущую тему на ${selectedTheme}?`);
+    if(!isConfirmed) {
+      themeSelect.value = lastSelectedTheme;
+      return;
+    }
+    setTheme(selectedTheme);
+    lastSelectedTheme = selectedTheme;
+  }
+
+  function setTheme (name) {
+    const selectedThemeObj = themes[name];
+    Object.entries(selectedThemeObj).forEach(([key, value]) => {
+      document.documentElement.style.setProperty(key, value);
+    });
+  }
+
+
+})(tasks);

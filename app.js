ddafecb24ded411a374cece1168fec2b8be0baad
Olga Garbuz <garbuz9112000@gmail.com>
2020-12-13 14:58:58 +0200

    news-app

--- app.js
+++ app.js
@@ -1,154 +1,199 @@
-//task 1
-console.log('task 1');
+//17fc59ad163c4ff0a2295b0980428f40
+// Custom Http Module
+function customHttp() {
+  return {
+    get(url, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('GET', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2)    {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
 
-(function (){
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
 
-    const map = ["_id", "name", "isActive", "balance"];
-
-    const users = [
-    {
-        "_id": "5d220b10e8265cc978e2586b",
-        "isActive": true,
-        "balance": 2853.33,
-        "age": 20,
-        "name": "Buckner Osborne",
-        "gender": "male",
-        "company": "EMPIRICA",
-        "email": "bucknerosborne@empirica.com",
-        "phone": "+1 (850) 411-2997",
-        "registered": "2018-08-13T04:28:45 -03:00"
+        xhr.send();
+      } catch (error) {
+        cb(error);
+      }
     },
-    {
-        "_id": "5d220b10144ef972f6c2b332",
-        "isActive": true,
-        "balance": 1464.63,
-        "age": 38,
-        "name": "Rosalie Smith",
-        "gender": "female",
-        "company": "KATAKANA",
-        "email": "rosaliesmith@katakana.com",
-        "phone": "+1 (943) 463-2496",
-        "registered": "2016-12-09T05:15:34 -02:00"
+    post(url, body, headers, cb) {
+      try {
+        const xhr = new XMLHttpRequest();
+        xhr.open('POST', url);
+        xhr.addEventListener('load', () => {
+          if (Math.floor(xhr.status / 100) !== 2) {
+            cb(`Error. Status code: ${xhr.status}`, xhr);
+            return;
+          }
+          const response = JSON.parse(xhr.responseText);
+          cb(null, response);
+        });
+
+        xhr.addEventListener('error', () => {
+          cb(`Error. Status code: ${xhr.status}`, xhr);
+        });
+
+        if (headers) {
+          Object.entries(headers).forEach(([key, value]) => {
+            xhr.setRequestHeader(key, value);
+          });
+        }
+
+        xhr.send(JSON.stringify(body));
+      } catch (error) {
+        cb(error);
+      }
     },
-    {
-        "_id": "5d220b1083a0494655cdecf6",
-        "isActive": false,
-        "balance": 2823.39,
-        "age": 40,
-        "name": "Estrada Davenport",
-        "gender": "male",
-        "company": "EBIDCO",
-        "email": "estradadavenport@ebidco.com",
-        "phone": "+1 (890) 461-2088",
-        "registered": "2016-03-04T03:36:38 -02:00"
+  };
+}
+// Init http module
+const http = customHttp();
+
+
+const newsService = (function(){
+  const apiKey = '17fc59ad163c4ff0a2295b0980428f40';
+  const apiUrl = 'https://news-api-v2.herokuapp.com';
+  return {
+    topHeadlines(country = 'ua', category = '', callback){
+      http.get(`${apiUrl}/top-headlines?category=${category}&country=${country}&apiKey=${apiKey}`, callback);
+    },
+    everything(query, callback){
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, callback)
     }
-    ];
-
-    const mapUsers = users.map(user => 
-        map.reduce((acc, key) => {
-            if (Object.keys(user).indexOf(key) !== -1) {
-                acc[key] = user[key];
-            }
-            return acc;
-        }, {}));
-
-    console.log(mapUsers);
-
-})();
-//task 2
-console.log('task 2');
-
-(function() {
-
-    const charWithIndex = [{char:"a",index:12}, {char:"w",index:8}, {char:"Y",index:10}, {char:"p",index:3}, {char:"p",index:2}, {char:"N",index:6}, {char:" ",index:5}, {char:"y",index:4}, {char:"r",index:13}, {char:"H",index:0}, {char:"e",index:11}, {char:"a",index:1}, {char:" ",index:9}, {char:"!",index:14}, {char:"e",index:7}];
-
-    function createStr(arrayOfChars){
-        if(Array.isArray(arrayOfChars)){
-            return [...arrayOfChars]
-                .sort((prevIndex, nextIndex) => prevIndex.index - nextIndex.index)
-                .reduce((acc, char) => (acc += char.char), '');
-        }
-        else console.log('Ошибочное значение');
-    }
-
-    const resultStr = createStr(charWithIndex);
-
-    console.log(resultStr);
-
+  };
 })();
 
-//task 3
-console.log('task 3');
+//Elements
+const newsContainer = document.querySelector('.news-container .row');
+const form = document.forms['newsControls'];
+const countrySelect = form.elements['country'];
+const categorySelect = form.elements['category'];
+const searchInput = form.elements['search'];
 
-(function() {
-    
-    function getInfo ({name = 'Unknown', info: {partners: [partner1, partner2] = []} = {}} = {}){
-        console.log(`Name: ${name} \nPartners: ${partner1}, ${partner2}`);
-    }
 
-    const organisation = { 
-        name: 'Google', 
-        info: { 
-          employees: ['Vlad', 'Olga'], 
-          partners: ['Microsoft', 'Facebook', 'Xing'] 
-      } };
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  loadNews();
+});
 
-    getInfo(organisation);
+//  init selects
+document.addEventListener('DOMContentLoaded', function() {
+  M.AutoInit();
+  loadNews();
+});
 
-})();
 
-//task 4
-console.log('task 4');
+// Load news function
+function loadNews(){
+  showLoader();
+  const { country, category, searchText } = getFormInput();
+  if(!searchText) {
+    newsService.topHeadlines(country, category, onGetResponse);
+  } else{
+    newsService.everything(searchText, onGetResponse);
+  }
+}
 
-(function() {  
+function getFormInput(){
+  return {
+    country: countrySelect.value,
+    category: categorySelect.value,
+    searchText: searchInput.value,
+  }
+}
 
-    const person = {
-        name: 'Denis',
-        age: 30,
-        lastGet: '',
-        lastUpdate: '', 
-        get nameField(){
-            this.lastGet = Date(Date.now());
-            console.log(this.name);
-        }, 
-        set nameField (newName) {
-            this.lastUpdate = Date(Date.now());
-            if(typeof newName === 'string') this.name = newName;
-            else console.log('Ошибочное значение');
-        }
-    };
-    console.log(person);
-    person.nameField;
-    console.log(person);
-    person.nameField = 'Olga';
-    console.log(person);
-})();
+// function on get response from server
+function onGetResponse(error, response) {
+  removePreLoader();
+  clearContainer(newsContainer);
 
-//task 5
-console.log('task 5');
+  if (error) {
+    showAlert(error,'error-msg');
+    return;
+  }
+  //show empty message
+  if (!response.articles.length){
+    newsContainer.insertAdjacentHTML('afterbegin', emptyMessageTemplate());
+    return;
+  }
+  renderNew(response.articles);
+}
 
-(function() {
-    
-    const product = {
-        brand: 'Apple',
-        model: 'iPhone 7',
-        price: '$300', 
-        get info() {
-            return `${this.brand} ${this.model}`;
-        },
-        set info(value) {
-            if(typeof value === 'string') {
-                [this.brand, ...model] = value.split(' ');
-                this.model = model.join(' ');
-            }
-            else console.log('Ошибочное значение');
-        },
-    };
-    console.log(product);
-    console.log(product.info);
-    product.info = 'Samsung S8 Gold';
-    console.log(product);
-    console.log(product.info);
+function renderNew(news) {
+  let fragment = '';
+  news.forEach(newsItem => {
+    const el = newsTemplate(newsItem);
+    fragment += el;
+  });
+  newsContainer.insertAdjacentHTML('afterbegin', fragment);
+}
 
-})();
+function clearContainer(container) {
+  let child = container.lastElementChild;
+  while (child) {
+    container.removeChild(child);
+    child = container.lastElementChild;
+  }
+}
+
+// News item template function 
+function newsTemplate({urlToImage, title, url, description}){
+  return `
+    <div class="col s12">
+      <div class="card"> 
+        <div class="card-image">
+          <img src="${urlToImage || 'no_image.png'}"/>
+          <span class="card-title"> ${title || ''} </span>
+        </div>  
+        <div class="card-content">
+          <p>${description || ''}</p>
+        </div>
+        <div class="card-action">
+          <a href="${url}">Read more </a>
+        </div>
+      </div>
+    </div>
+  `;
+}
+
+function emptyMessageTemplate(){
+  return `
+  <div class="col s12 bold">
+    <div class="card"> 
+      <div class="card-content">
+        <p> К сожалению, по вашему запросу ничего не найдено. Попробуйте изменить поисковый запрос :)</p>
+      </div>
+    </div>
+  </div>
+`;
+}
+
+
+
+function showAlert(message, type = 'success') {
+  M.toast({html: message, classes:type});
+}
+
+function showLoader(){
+  document.body.insertAdjacentHTML(
+    'afterbegin', 
+    `<div class="progress">
+      <div class="indeterminate"></div>
+    </div>`);
+}
+
+function removePreLoader() {
+  const loader = document.querySelector('.progress');
+  if (loader) {
+    loader.remove();
+  }
+}
 

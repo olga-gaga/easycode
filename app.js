8bb4febc7f32fd408150f847f3f3a258d26e76b2
Olga Garbuz <garbuz9112000@gmail.com>
2020-12-24 14:00:06 +0200

    news app using async await and fetch

--- app.js
+++ app.js
@@ -1,57 +1,29 @@
-//17fc59ad163c4ff0a2295b0980428f40
 // Custom Http Module
-function customHttp() {
+function customHttp () {
   return {
-    get(url, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', url);
-        xhr.addEventListener('load', () => {
-          if (Math.floor(xhr.status / 100) !== 2)    {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
+      async get(url) {
+          try{
+              response = await fetch(url).then(res => res.json());
+              return response;
           }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
-
-        xhr.addEventListener('error', () => {
-          cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
-
-        xhr.send();
-      } catch (error) {
-        cb(error);
-      }
-    },
-    post(url, body, headers, cb) {
-      try {
-        const xhr = new XMLHttpRequest();
-        xhr.open('POST', url);
-        xhr.addEventListener('load', () => {
-          if (Math.floor(xhr.status / 100) !== 2) {
-            cb(`Error. Status code: ${xhr.status}`, xhr);
-            return;
+          catch(error) {
+              return Promise.reject(error);
           }
-          const response = JSON.parse(xhr.responseText);
-          cb(null, response);
-        });
-
-        xhr.addEventListener('error', () => {
-          cb(`Error. Status code: ${xhr.status}`, xhr);
-        });
-
-        if (headers) {
-          Object.entries(headers).forEach(([key, value]) => {
-            xhr.setRequestHeader(key, value);
-          });
-        }
-
-        xhr.send(JSON.stringify(body));
-      } catch (error) {
-        cb(error);
+      },
+      async post(url, headers = {}, body = {}) {
+          try{
+              const init = {
+                  method:'POST',
+                  headers: headers,
+                  body: JSON.stringify(body),
+              }
+              const response = await fetch(url, init).then(res => res.json());
+              return response;
+          }
+          catch (error) {
+              return Promise.reject(error);
+          }
       }
-    },
   };
 }
 // Init http module
@@ -63,10 +35,14 @@ const newsService = (function(){
   const apiUrl = 'https://news-api-v2.herokuapp.com';
   return {
     topHeadlines(country = 'ua', category = '', callback){
-      http.get(`${apiUrl}/top-headlines?category=${category}&country=${country}&apiKey=${apiKey}`, callback);
+      http.get(`${apiUrl}/top-headlines?category=${category}&country=${country}&apiKey=${apiKey}`)
+        .then(response => callback(null, response))
+        .catch(error => callback(error));
     },
     everything(query, callback){
-      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`, callback)
+      http.get(`${apiUrl}/everything?q=${query}&apiKey=${apiKey}`)
+        .then(response => callback(null, response))
+        .catch(error => callback(error));
     }
   };
 })();
@@ -90,8 +66,6 @@ document.addEventListener('DOMContentLoaded', function() {
   loadNews();
 });
 
-
-// Load news function
 function loadNews(){
   showLoader();
   const { country, category, searchText } = getFormInput();
@@ -110,7 +84,6 @@ function getFormInput(){
   }
 }
 
-// function on get response from server
 function onGetResponse(error, response) {
   removePreLoader();
   clearContainer(newsContainer);
@@ -128,11 +101,7 @@ function onGetResponse(error, response) {
 }
 
 function renderNew(news) {
-  let fragment = '';
-  news.forEach(newsItem => {
-    const el = newsTemplate(newsItem);
-    fragment += el;
-  });
+  const fragment = news.reduce( (acc, newsItem) => acc += newsTemplate(newsItem), '');
   newsContainer.insertAdjacentHTML('afterbegin', fragment);
 }
 
@@ -144,7 +113,6 @@ function clearContainer(container) {
   }
 }
 
-// News item template function 
 function newsTemplate({urlToImage, title, url, description}){
   return `
     <div class="col s12">
@@ -175,8 +143,6 @@ function emptyMessageTemplate(){
   </div>
 `;
 }
-
-
 
 function showAlert(message, type = 'success') {
   M.toast({html: message, classes:type});
